35b25fe675b69ae724b2a60dcc62c34c51d93f6f
nihal <nihal.jupalli09@gmail.com>
2025-08-02 11:39:04 -0400

    file opener and glfw/glad init code

--- main.cpp
+++ main.cpp
@@ -3,7 +3,8 @@
 #include <windows.h>
 #include <commdlg.h>
 #include <fstream>
-
+#include <glad/gl.h>
+#include <GLFW/glfw3.h>
 std::wstring OpenFileDialog() {
     wchar_t filename[MAX_PATH] = L"";
 
@@ -19,26 +20,86 @@ std::wstring OpenFileDialog() {
     } else return L"";
 }
 
+
+// function updates the viewport size if user resizes window
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+    glViewport(0, 0, width, height);
+}
 int main() {
-    std::wstring filepath = OpenFileDialog();
-    if (filepath.empty()) {
-        std::cout<<"no file selected\n";
-        return 0;
-    }
 
-    std::string path(filepath.begin(), filepath.end());
-    std::ifstream file(path);
+    //one-time GLFW and GLAD setup code
+        if (!glfwInit()) {
+            std::cerr << "glfw didn't init";
+            return 1;
+        }
 
-    if (!file) {
-        std::cerr << "couldn't open file\n";
-        return 1;
-    }
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-    std::string line;
+        GLFWwindow* window = glfwCreateWindow(800,600, "gcode visualization", nullptr, nullptr);
 
-    while (std::getline(file,line)) {
-        std::cout << line << "\n";
-    }
+        if (!window) {
+            std::cerr << "failed to load window";
+            glfwTerminate();
+        }
+
+        glfwMakeContextCurrent(window);
+
+        if (!gladLoadGL((GLADloadfunc)glfwGetProcAddress)) {
+            std::cerr << "Failed to initialize GLAD\n";
+            return -1;
+        }
+        glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+        glViewport(0,0,800,600); //change window dimensions here
+
+
+        //loop until user closes window
+        while (!glfwWindowShouldClose(window)) {
+            glClearColor(0.1f, 0.1f, 0.2f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+        
+
+
+    //parsing for only G0/G1/G2/G3
+        std::wstring filepath = OpenFileDialog();
+        if (filepath.empty()) {
+            std::cout<<"no file selected\n";
+            return 0;
+        }
+
+        std::string path(filepath.begin(), filepath.end());
+        std::ifstream file(path);
+        std::ofstream parsedFile("C:\\Users\\nihal\\OneDrive\\Desktop\\code\\gcode-parser\\parsedFile.txt");
+
+        //error handling
+        if (!parsedFile) {
+            std::cerr << "couldnt open output file";
+            return 1;
+        }
+        if (!file) {
+            std::cerr << "couldn't open file\n";
+            return 1;
+        }
+
+        std::string line;
+        while (std::getline(file,line)) {
+            std::string prefix = line.substr(0,2);
+            if (prefix == "G0" || prefix == "G1" || prefix == "G2" || prefix == "G3") {
+                parsedFile << line;
+                parsedFile << "\n";
+            }
+        }
+
+
+
+
+
+
+    //close files
     file.close();
-
+    parsedFile.close();
 }
